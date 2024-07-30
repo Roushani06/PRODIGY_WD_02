1565a545e0f6154c5e1e0c3e8a97623f6d3123e8
Roushani06 <roushani77100@gmail.com>
2024-07-30 19:16:12 +0530

    SCRIPT.JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let timerDisplay = document.querySelector('.time');
-let stopBtn = document.querySelector('#stopBtn');
-let startBtn = document.querySelector('#startBtn');
-let resetBtn = document.querySelector('#resetBtn');
+let timerDisplay = document.querySelector(".time");
+let stopBtn = document.querySelector("#stopBtn");
+let startBtn = document.querySelector("#startBtn");
+let resetBtn = document.querySelector("#resetBtn");
 
 let msec = 00;
 let secs = 00;
@@ -9,42 +9,43 @@ let mins = 00;
 
 let timer = null;
 
-startBtn.addEventListener('click', ()=>{
-    if(timer!== null){
-        clearInterval(timer);
-    }
-    timer = setInterval(startTimer, 10);
-});
-
-stopBtn.addEventListener('click', ()=>{
+startBtn.addEventListener("click", () => {
+  if (timer !== null) {
     clearInterval(timer);
-});
-
-resetBtn.addEventListener('click', ()=>{
-    clearInterval(timer);
-    timerDisplay.innerHTML = `00 : 00 : 00`;
-    msec = secs = mins = 00;
+  }
+  timer = setInterval(startTimer, 10);
 });
 
 
-function startTimer(){
-    msec++;
-    if(msec == 100){
-        msec = 0;
-        secs++;
-        if(secs == 60){
-            secs = 0;
-            mins++;
 
-        }
+stopBtn.addEventListener("click", () => {
+  clearInterval(timer);
+});
+
+
+
+resetBtn.addEventListener("click", () => {
+  clearInterval(timer);
+  timerDisplay.innerHTML = `00 : 00 : 00`;
+  msec = secs = mins = 00;
+});
+
+
+
+function startTimer() {
+  msec++;
+  if (msec == 100) {
+    msec = 0;
+    secs++;
+    if (secs == 60) {
+      secs = 0;
+      mins++;
     }
+  }
 
-    let secString = secs < 10 ? `0${secs}`: secs;
-    let msecString = msec < 10 ? `0${msec}`: msec;
-    let minString = mins < 10 ? `0${mins}`: mins;
+  let secString = secs < 10 ? `0${secs}` : secs;
+  let msecString = msec < 10 ? `0${msec}` : msec;
+  let minString = mins < 10 ? `0${mins}` : mins;
 
-    timerDisplay.innerHTML = `${minString} : ${secString} : ${msecString}`;
-
-
+  timerDisplay.innerHTML = `${minString} : ${secString} : ${msecString}`;
 }
-
